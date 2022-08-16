f225615ea0e4afd923a9c44b471680833457bba0
Krishnanandh shaji <krishnanandhshaji@gmail.com>
2022-08-16 13:39:13 +0530

    style updated

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style/style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=Git , initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    Git Example
-    first commit
-    <br>
+<section>    
+    <div>Git Example</div>
+    <div>first commit</div>
+</section>
 </body>
 </html>
